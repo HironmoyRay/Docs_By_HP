4d3a47b08cf4ba66a607c8d530d040b1d1d2b483
hironmoy-ray <hpray35@gmail.com>
2024-03-10 13:07:57 +0600

    Italic & Underline Configure

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 document.getElementById("btn-bold").addEventListener("click", function () {
   const textField = document.getElementById("text-field");
-  const bold = textField.classList.add('bold');
-  console.log("Btn Clicked");
+  const bold = textField.classList.add("bold");
+  //   console.log("Btn Clicked");
+});
+document.getElementById("btn-italic").addEventListener("click", function () {
+  const textField = document.getElementById("text-field");
+  const italic = textField.classList.add("italic");
+  //   console.log("Btn Clicked");
+});
+document.getElementById("btn-underline").addEventListener("click", function () {
+  const textField = document.getElementById("text-field");
+  const underline = textField.classList.add("underline");
+  //   console.log("Btn Clicked");
 });
