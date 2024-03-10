e5040fe00dcfb09c3d42eafcf3a8288f927a13ff
hironmoy-ray <hpray35@gmail.com>
2024-03-10 13:19:44 +0600

    Alignment Configure

--- js/script.js
+++ js/script.js
@@ -13,3 +13,23 @@ document.getElementById("btn-underline").addEventListener("click", function () {
   const underline = textField.classList.add("underline");
   //   console.log("Btn Clicked");
 });
+document.getElementById("left").addEventListener("click", function () {
+  const textField = document.getElementById("text-field");
+  const left = textField.classList.add("left-align");
+  //   console.log("Btn Clicked");
+});
+document.getElementById("right").addEventListener("click", function () {
+  const textField = document.getElementById("text-field");
+  const right = textField.classList.add("right-align");
+  //   console.log("Btn Clicked");
+});
+document.getElementById("center").addEventListener("click", function () {
+  const textField = document.getElementById("text-field");
+  const center = textField.classList.add("center-align");
+  //   console.log("Btn Clicked");
+});
+document.getElementById("justify").addEventListener("click", function () {
+  const textField = document.getElementById("text-field");
+  const justify = textField.classList.add("justify");
+  //   console.log("Btn Clicked");
+});
